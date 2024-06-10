59edbd06f14d83a5ab5bfca664362df46357a290
stdbob <bobber1@yahoo.com>
2024-06-10 19:21:40 +0300

    data compose 2

--- service/src/main/java/bootiful/service/ServiceApplication.java
+++ service/src/main/java/bootiful/service/ServiceApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
 
 @SpringBootApplication
 public class ServiceApplication {
@@ -17,5 +22,20 @@ public class ServiceApplication {
 record Customer(@Id Integer id, String name){}
 
 interface CustomerRepository extends ListCrudRepository<Customer,Integer> {
+}
 
+@Controller
+@ResponseBody
+class CustomerController {
+
+	private final CustomerRepository repository;
+
+	CustomerController(CustomerRepository repository){
+		this.repository = repository;
+	}
+
+	@GetMapping("/customers")
+	Collection<Customer> customers() {
+		return  repository.findAll();
+	}
 }
