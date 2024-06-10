aca4935fbb2ea023f890255672cec19896d5cfb9
stdbob <bobber1@yahoo.com>
2024-06-10 19:14:09 +0300

    data compose

--- service/src/main/java/bootiful/service/ServiceApplication.java
+++ service/src/main/java/bootiful/service/ServiceApplication.java
@@ -3,6 +3,7 @@ package bootiful.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.ListCrudRepository;
 
 @SpringBootApplication
 public class ServiceApplication {
@@ -13,18 +14,8 @@ public class ServiceApplication {
 
 }
 
-sealed interface Loan permits SecuredLoan, UnsecuredLoan {}
-
-final class SecuredLoan implements Loan {}
-
-record UnsecuredLoan(float interest) implements Loan {}
-
-class Message {
-	String messageForLoan(Loan l){
-		return switch (l) {
-			case UnsecuredLoan (var interest) -> "oh! too bad. that interest rate is high! " + interest;
-			case SecuredLoan sl -> "good job on getting a secured loan!";
-		};
-	}
-}
 record Customer(@Id Integer id, String name){}
+
+interface CustomerRepository extends ListCrudRepository<Customer,Integer> {
+
+}
---
+++ service/src/main/resources/schema.sql
@@ -0,0 +1,4 @@
+create table if not exists customer (
+    id serial primary key not null,
+    name text not null
+);
