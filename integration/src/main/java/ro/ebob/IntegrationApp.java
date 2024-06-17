90e122f36a0a75989901f8c2e052f6e7baac3c0e
stdbob <bobber1@yahoo.com>
2024-06-17 20:20:04 +0300

    [integration] MessagingGateway

--- integration/src/main/java/ro/ebob/IntegrationApp.java
+++ integration/src/main/java/ro/ebob/IntegrationApp.java
@@ -2,16 +2,18 @@ package ro.ebob;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.core.GenericHandler;
 import org.springframework.integration.core.GenericSelector;
 import org.springframework.integration.core.GenericTransformer;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.context.IntegrationFlowContext;
+import org.springframework.integration.dsl.MessageChannels;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
@@ -20,7 +22,6 @@ import org.springframework.stereotype.Component;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
-import java.util.Set;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -40,15 +41,6 @@ public class IntegrationApp {
         };
     }
 
-    @Bean
-    ApplicationRunner runFlow(MyMessageSource myMessageSource, IntegrationFlowContext context){
-        return args -> {
-            IntegrationFlow flow1 = buildFlow(myMessageSource, "Hola", Duration.ofSeconds(1));
-            IntegrationFlow flow2 = buildFlow(myMessageSource, "Hello", Duration.ofSeconds(2));
-            Set.of(flow1, flow2).forEach(flow -> context.registration(flow).register().start());
-        };
-    }
-
     @Component
     static class MyMessageSource implements MessageSource<String> {  //poll or wait for events
 
@@ -62,17 +54,44 @@ public class IntegrationApp {
         }
     }
 
-    private static IntegrationFlow buildFlow(MyMessageSource myMessageSource, String filterText, Duration fixedRate) {
+    @Bean
+    MessageChannel greetings() {
+        return MessageChannels.direct().getObject();
+    }
+
+    @Bean
+    IntegrationFlow flow(MyMessageSource myMessageSource) {
         return IntegrationFlow
-                .from(myMessageSource, spec -> spec.poller(pollerFactory -> pollerFactory.fixedRate(fixedRate)))
-                .filter(String.class, (GenericSelector<String>) source -> source.contains(filterText))
+                .from(greetings())
+                .filter(String.class, (GenericSelector<String>) source -> source.contains("Hola"))
                 .transform((GenericTransformer<String, String>) String::toUpperCase)
                 //handler(GenericHandler returning null acts like a filter)
                 //.channel(atob())
                 .handle((GenericHandler<String>) (payload, headers) -> {
-                    LOG.info("The payload is {}", payload);
+                    LOG.info("RESULT:: {}", payload);
                     return null;//terminates the pipeline
                 })
                 .get();
     }
+}
+
+@Component
+class Runner implements ApplicationRunner {
+
+    final GreetingsClient gateway;
+
+    Runner(GreetingsClient gateway) {
+        this.gateway = gateway;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        gateway.greet("Hola bla bla");
+        gateway.greet("Hello alsdsd");
+    }
+}
+
+@MessagingGateway(defaultRequestChannel = "greetings")
+interface GreetingsClient {
+    void greet(String text);
 }
